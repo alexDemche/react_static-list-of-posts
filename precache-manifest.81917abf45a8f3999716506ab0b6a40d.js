9970be7a159758151d17723fa103ab336cc0a487
alexDemche <alexeyydemchenko@gmail.com>
2019-09-08 20:23:08 +0300

    Updates

---
+++ precache-manifest.81917abf45a8f3999716506ab0b6a40d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25293111546fcc7da9f890472a9a91f2",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "86399c7761c00f7ed5e8",
+    "url": "/react_static-list-of-posts/static/css/main.ec9505b9.chunk.css"
+  },
+  {
+    "revision": "fa67027c564020748778",
+    "url": "/react_static-list-of-posts/static/js/2.78963138.chunk.js"
+  },
+  {
+    "revision": "86399c7761c00f7ed5e8",
+    "url": "/react_static-list-of-posts/static/js/main.4217040d.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
